0d1feab7441733767e3dda3a0954904c7296c4e3
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-06-24 22:56:13 +0530

    feat/home section initial

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/utkarsh-garg-09/" className="home__social-icon" target='_blank'>
+            <i className="uil uil-linkedin"></i>
+        </a>
+        <a href="https://github.com/utkarsh0903" className="home__social-icon" target='_blank'>
+            <i className="uil uil-github-alt"></i>
+        </a>
+        <a href="https://dribbble.com/" className="home__social-icon" target='_blank'>
+            <i className="uil uil-instagram"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
