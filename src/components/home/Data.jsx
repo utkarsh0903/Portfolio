48018ce211b33e0522e1da7b105e69740fa1aa66
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-06-24 23:58:05 +0530

    feat/home section responsive

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -58,7 +58,7 @@ const Data = () => {
       <h3 className="home__subtitle">Frontend Developer</h3>
       <p className="home__description">
         I'm creative developer based in New Delhi, and I'm very passionate and
-        dedicated to my work
+        dedicated to my work.
       </p>
       <a href="#contact" className="button button--flex">
         Say Hello
