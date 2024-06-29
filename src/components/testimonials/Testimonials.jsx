9d36fe8024d2c7997c2b0618608f113500871ce3
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-06-29 13:11:00 +0530

    feat/testimonial section

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./testimonials.css";
+import { Data } from "./Data";
+
+const Testimonials = () => {
+  return (
+    <section className="testimonial section container">
+      <h2 className="section__title">My clients say</h2>
+      <span className="section__subtitle">Testimonial</span>
+      {Data.map(({ id, image, title, description }) => {
+        return (
+          <div className="testimonial__card" key={id}>
+            <img src={image} alt="" className="testimonial__img" />
+            <h3 className="testimonial__name">{title}</h3>
+            <p className="testimonial__description">{description}</p>
+          </div>
+        );
+      })}
+      <div className="testiminial__container"></div>
+    </section>
+  );
+};
+
+export default Testimonials;
