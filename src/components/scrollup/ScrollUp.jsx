32d44df1cb156d8ea38bd3b8592f0760aae24335
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-07-03 01:08:20 +0530

    feat/header active link

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -4,6 +4,8 @@ import "./scrollup.css";
 const ScrollUp = () => {
   window.addEventListener("scroll", function () {
     const scrollUp = this.document.querySelector(".scrollup");
+    // When the scroll is higher than 560 viewport height, add the
+    //  show-scroll class to a tag with the scroll top class.
     if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
     else scrollUp.classList.remove("show-scroll")
   });
